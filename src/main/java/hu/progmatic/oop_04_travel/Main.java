478d0fa4670fd468ab27cc7785dc96d64ee33ea0
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-09 19:14:03 +0200

    bm: update

--- src/main/java/hu/progmatic/oop_04_travel/Main.java
+++ src/main/java/hu/progmatic/oop_04_travel/Main.java
@@ -1,7 +1,26 @@
 package hu.progmatic.oop_04_travel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Travel> travels = new ArrayList<>();
 
+        String data;
+
+        System.out.println("Kérem az utazások adatait, ha végeztél, akkor írjál egy üres sort.");
+
+        do {
+            data = scanner.nextLine();
+        } while (!data.isBlank());
+
+        System.out.println("A megadott utazások adatai:");
+
+        for (Travel travel : travels) {
+            System.out.println(travel);
+        }
     }
 }
