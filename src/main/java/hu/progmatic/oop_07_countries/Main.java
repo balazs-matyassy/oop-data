72be4648094b369839091e591b628bb6301906eb
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-16 17:11:40 +0200

    bm: update

---
+++ src/main/java/hu/progmatic/oop_07_countries/Main.java
@@ -0,0 +1,10 @@
+package hu.progmatic.oop_07_countries;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Kérjünk be szóközzel elválasztva ország - főváros párokat.
+        // 2. Akkor fejezzük be a beolvasást, ha a felhasználó üres sort ad meg.
+        // 3. Írjuk ki az adatbázis méretét.
+        // 4. Kérjük be egy ország nevét, írjuk ki a fővárosát.
+    }
+}
