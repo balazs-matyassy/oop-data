91149408f9a09ea079f91d22477f3586828108ee
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-16 17:20:04 +0200

    bm: update

--- src/main/java/hu/progmatic/oop_07_countries/Main.java
+++ src/main/java/hu/progmatic/oop_07_countries/Main.java
@@ -2,9 +2,28 @@ package hu.progmatic.oop_07_countries;
 
 public class Main {
     public static void main(String[] args) {
+        // -----
+        // 1. FELADAT
+        // -----
         // 1. Kérjünk be szóközzel elválasztva ország - főváros párokat.
         // 2. Akkor fejezzük be a beolvasást, ha a felhasználó üres sort ad meg.
         // 3. Írjuk ki az adatbázis méretét.
         // 4. Kérjük be egy ország nevét, írjuk ki a fővárosát.
+
+        // -----
+        // 2. FELADAT
+        // -----
+        // TÉNYLEGESEN ÉRDEMES LENNE A CITY OSZTÁLYBAN ELMENTENI AZ ORSZÁGOT.
+        // NEM SZÉP EZT EGY KÜLÖN MAPBEN TÁROLNI, DE GYAKORLÁS SZEMPONTJÁBÓL JÓ.
+        // -----
+        // 1. Kérjünk be szóközzel elválasztva város - ország párokat.
+        // 2. Akkor fejezzük be a beolvasást, ha a felhasználó üres sort ad meg.
+        // 3. Írjuk ki az adatbázis méretét.
+        // 4. Írjuk ki a különböző nevű városokat növekvő sorrendben.
+        // 5. Hozzunk létre egy City osztályt, az osztály a név mellett tartalmazzon egy egy mezőt is.
+        // 6. Tároljuk el a beolvasott értékeket egy City -> String Map-be. (Amúgy nem ez a legszebb megoldás).
+        // 7. Kérjünk be a felhasználótól egy id-t.
+        // 8. Hozzunk létre egy egy üres (csak id-t tartalmazó, nevet nem tartalmazó City példányt).
+        // 9. Az üres példány segítségével keressük meg a mapben a városhoz tartozó országot.
     }
 }
