6ce75182dfc590342417c42744eb2a5d6cdeed62
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-16 18:06:22 +0200

    bm: update

--- src/main/java/hu/progmatic/oop_07_countries/Main.java
+++ src/main/java/hu/progmatic/oop_07_countries/Main.java
@@ -1,5 +1,7 @@
 package hu.progmatic.oop_07_countries;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         // -----
@@ -9,6 +11,27 @@ public class Main {
         // 2. Akkor fejezzük be a beolvasást, ha a felhasználó üres sort ad meg.
         // 3. Írjuk ki az adatbázis méretét.
         // 4. Kérjük be egy ország nevét, írjuk ki a fővárosát.
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> countries = new TreeMap<>();
+
+        String line;
+
+        do {
+            System.out.println("Kérem a következő adatot.");
+            line = scanner.nextLine();
+
+            if (!line.isBlank()) {
+                String[] parts = line.split(" ");
+                countries.put(parts[0], parts[1]);
+            }
+        } while (!line.isBlank());
+
+        System.out.println("Az adatbázis mérete: " + countries.size());
+
+        System.out.println("Kérek egy országot.");
+        String country = scanner.nextLine();
+
+        System.out.println("A megadott ország fővárosa: " + countries.get(country));
 
         // -----
         // 2. FELADAT
@@ -20,10 +43,39 @@ public class Main {
         // 2. Akkor fejezzük be a beolvasást, ha a felhasználó üres sort ad meg.
         // 3. Írjuk ki az adatbázis méretét.
         // 4. Írjuk ki a különböző nevű városokat növekvő sorrendben.
-        // 5. Hozzunk létre egy City osztályt, az osztály a név mellett tartalmazzon egy egy mezőt is.
+        // 5. Hozzunk létre egy City osztályt, az osztály a név mellett tartalmazzon egy id mezőt is.
         // 6. Tároljuk el a beolvasott értékeket egy City -> String Map-be. (Amúgy nem ez a legszebb megoldás).
         // 7. Kérjünk be a felhasználótól egy id-t.
         // 8. Hozzunk létre egy egy üres (csak id-t tartalmazó, nevet nem tartalmazó City példányt).
         // 9. Az üres példány segítségével keressük meg a mapben a városhoz tartozó országot.
+        Map<City, String> cities = new HashMap<>();
+        Set<String> cityNames = new TreeSet<>();
+
+        do {
+            System.out.println("Kérem a következő adatot.");
+            line = scanner.nextLine();
+
+            if (!line.isBlank()) {
+                String[] parts = line.split(" ");
+                cityNames.add(parts[1]);
+                // a kulcs egy összetett objektum
+                // az equals és hashCode alapján lehet a Map-ben keresni
+                cities.put(new City(Integer.parseInt(parts[0]), parts[1]), parts[2]);
+            }
+        } while (!line.isBlank());
+
+        System.out.println("Adatbázis mérete: " + cities.size());
+
+        System.out.println("Különböző városok:");
+
+        for (String cityName : cityNames) {
+            System.out.println(cityName);
+        }
+
+        System.out.println("Kérem az azonosítót.");
+        int id = scanner.nextInt();
+        City city = new City(id);
+
+        System.out.println("A megadott városhoz tartozó ország: " + cities.get(city));
     }
 }
