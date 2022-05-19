297c6fc25e143a1b7137828a4450f1f1f158d095
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-19 18:55:35 +0200

    bm: update

---
+++ src/main/java/hu/progmatic/oop_11_quartals/Main.java
@@ -0,0 +1,20 @@
+package hu.progmatic.oop_11_quartals;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Hozzunk létre egy Quartal osztályt.
+        // 2. Az osztály rendelkezzen egy income és egy expenditure mezővel.
+        // 3. Legyen egy számított profit mező (income - expenditure)
+        // 4. Hozzuk létre a gettereket + settereket + toStringet
+        // 5. Legyen 3 konstruktor:
+        //      - default
+        //      - income és expenditure
+        //      - egy szóközzel elválasztott sort váró
+        // 6. Olvassuk be a quartals.txt tartalmát és tároljuk el egy List<Quartal> adatstruktúrába.
+        // 7. Írjuk ki az adatok számát.
+        // 8. Határozzuk meg a legnagyobb nyereséget.
+        // 9. Határozzuk meg a legnagyobb veszteséget.
+        // 10. Hány veszteséges negyedév volt?
+        // 11. Mennyi volt az összes bevétel?
+    }
+}
