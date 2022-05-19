4dac730ee9102e96280739ddc3a009cabad3fb87
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-19 18:04:03 +0200

    bm: update

--- src/main/java/hu/progmatic/oop_09_quartals/Main.java
+++ src/main/java/hu/progmatic/oop_09_quartals/Main.java
@@ -1,13 +1,34 @@
 package hu.progmatic.oop_09_quartals;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("quartals.txt"));
+            // A System.in egy InputStream
+            // InputStream: valamilyen adatforrást enged byteonként kiolvasni
+            // FileInputStream, ByteArrayInputStream (konstans tömböt ad vissza -> lsd. StringReader)
+
+            BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Kérek egy sort!");
+            String inLine = inReader.readLine();
+            System.out.println("A megadott sor: " + inLine);
+
+
+            // Reader: valamilyen adatforrást enged karakterenként kiolvasni
+            //  - FileReader: szöveges fájlokat enged karakterenként kiolvasni
+            //  - StringReader: fix szöveget ad vissza karakterenként
+            //  - InputStreamReader: Egy InputStream-ből olvas ki egy byteot,
+            //  a kiolvasott értéket karakterré alakítva adja vissza
+            // BufferedReader: olyan Reader, akinek van egy beépített puffere
+            //      képes összegyűjteni több karaktert
+            //      pl. Képesek vagyunk egy teljes sort kiolvasni.
+            //          readLine() !!!
+            //      Nem önállóan működik, hanem egy másik Readert "csomagol be".
+            // BufferedReader reader = new BufferedReader(new FileReader("c:\\quartals.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("data\\quartals.txt"));
+            // BufferedReader reader = new BufferedReader(new StringReader("Hello\nWorld"));
 
             String line;
 
@@ -15,7 +36,7 @@ public class Main {
                 System.out.println(line);
             }
 
-            reader.close();
+            reader.close(); // ez nem teljesen szép
         } catch (IOException e) {
             e.printStackTrace();
         }
